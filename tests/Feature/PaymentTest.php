5d603808181daa6272928dea5f081a47783b89ae
padaelocomunity2 <sahrulalfa45@gmail.com>
2021-01-23 14:50:37 +0800

    perbaikan test

--- tests/Feature/PaymentTest.php
+++ tests/Feature/PaymentTest.php
@@ -23,27 +23,26 @@ class PaymentTest extends TestCase
 
     public function testStoreOneField()
     {
-    $countPayment = Payment::count();
-    if($countPayment <1){
         $payments = \App\Models\Payment::factory()->create();
         $this->post('/payments',$payments->toArray());
         $this->assertEquals(1,Payment::all()->count());
-    }
-    $this->assertTrue(true);
+
     }
 
     public function testfieldData()
     {
-        $paymentName = 'BNI';
 
-        $payment = new Payment();
-        $payment->payment_name = $paymentName;
-        $payment->save();
+            $paymentName = 'BNI';
 
-        // Melihat data BNI ada tidak di database
-        $this->assertDatabaseHas('payments', [
-            'payment_Name' => 'BNI'
-         ]);
+            $payment = new Payment();
+            $payment->payment_name = $paymentName;
+            $payment->save();
+
+            // Melihat data BNI ada tidak di database
+            $this->assertDatabaseHas('payments', [
+                'payment_Name' => 'BNI'
+             ]);
+
     }
 
     public function testDeleteEmptyId()
